58af67b86a0ea3cd113d3f32324be05b9078da95
Vincent Bauchart <vincent-externe.bauchart@dalkia.fr>
2026-03-24 11:00:10 +0100

    no full screen

--- app/src/main/java/com/terrass/app/MainActivity.kt
+++ app/src/main/java/com/terrass/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.terrass.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.terrass.app.ui.TerassApp
 import com.terrass.app.ui.theme.TerassTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TerassTheme {
                 TerassApp()
